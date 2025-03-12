939fe2780c1d66815acea63f805bd230354f202d
kshitij <kshitijbarman1234@gmail.com>
2025-03-12 17:38:17 +0530

    route added

--- Ecommerce-App/Frontend/src/App.jsx
+++ Ecommerce-App/Frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-5[vw] md:px-[7vw] lg:px-[9vw]'>App</div>
   )
 }
 
